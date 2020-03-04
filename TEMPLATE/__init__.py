0d2b310278192e59afa4f8defbfe3ddc33503d5d
Guido Imperiale <crusaderky@gmail.com>
2020-03-04 14:54:51 +0000

    pkg_resources tweaks

--- TEMPLATE/__init__.py
+++ TEMPLATE/__init__.py
@@ -7,7 +7,7 @@ try:
     __version__ = pkg_resources.get_distribution("TEMPLATE").version
 except Exception:
     # Local copy, not installed with setuptools
-    __version__ = "unknown"
+    __version__ = "999"
 
 
 __all__ = ("__version__", "hello")
