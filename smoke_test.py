c9239ca7cb7c112d7b24b90a30899f75472634f0
Guido Imperiale <crusaderky@gmail.com>
2025-10-02 23:27:46 +0200

    Reinstate MacIntel tests (#54)

--- smoke_test.py
+++ smoke_test.py
@@ -1,10 +1,12 @@
 """Test import the library and print essential information"""
 
+import platform
 import sys
 
 import TEMPLATE
 
 print("Python interpreter:", sys.executable)
 print("Python version    :", sys.version)
+print("Platform          :", platform.platform())
 print("Library path      :", TEMPLATE.__file__)
 print("Library version   :", TEMPLATE.__version__)
